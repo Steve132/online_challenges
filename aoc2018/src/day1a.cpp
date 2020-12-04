72d22f9df84d893b570304f253f963bd5bcf39c6
Steve132 <steve@soapforge.com>
2020-12-03 21:23:31 -0500

    Day1+macros

--- aoc2018/src/day1a.cpp
+++ aoc2018/src/day1a.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
 #include "aoc.hpp"
+#include <iterator>
+#include <algorithm>
+#include <numeric>
+#include <cstdint>
 
 int main(int argc,char** argv)
 {
+	system("pwd");
 	auto input=aoc::load_input("day1",argc,argv);
+
+	int current_frequency=std::accumulate((std::istream_iterator<int>(*input)),
+						  std::istream_iterator<int>(),
+						  0);
+	
+	std::cout << "The current frequency is: " << current_frequency << std::endl;
+	
 	return 0;
 }
