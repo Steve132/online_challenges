af6100d1a6e5b9d685c0572d850ac4819f66b005
Steve132 <steve@soapforge.com>
2023-01-10 11:12:14 -0500

    Day5

--- aoc2022/day05/main_b.cpp
+++ aoc2022/day05/main_b.cpp
@@ -7,50 +7,115 @@
 #include<algorithm>
 #include<unordered_set>
 #include<array>
+#include<stack>
+#include<string>
 
-struct Range{
-    size_t start,finish;
 
-    bool contains(const Range& r) const{
-        return start <= r.start && r.finish <= finish; 
-    }
-    bool overlap(const Range& r) const{
-        return start <= r.finish && finish >= r.start;
-    }
+struct Move{
+    size_t count,source,destination;
 };
-struct Assignment: public std::array<Range,2>
-{
-
-};
-std::istream& operator>>(std::istream& inp,Range& g){
-    size_t a,b;
-    std::string m;
-    inp >> a;
-    inp.ignore();
-    inp >> b;
-    g=Range{a,b};
-    return inp;
-}
-std::ostream& operator<<(std::ostream& out,const Range& g){
-    return out << g.start << "-" << g.finish;
-}
-std::istream& operator>>(std::istream& inp,Assignment& a){
-    inp >> a[0];
-    inp.ignore();
-    inp >> a[1];
+std::istream& operator>>(std::istream& inp,Move& m){
+    std::string t;
+    m=Move{};
+    inp >> t >> m.count >> t >> m.source >> t >> m.destination;
+    m.source-=1;m.destination-=1;
     return inp;
 }
 
-const std::string INPUTFILE="../day04/input.in";
-const std::string TESTFILE="../day04/test.in";
+struct Crane{
+    std::vector<std::vector<char>> stacks;
+    void apply(const Move& m){
+        auto rb=stacks[m.source].begin()+stacks[m.source].size()-m.count;
+        std::copy_n(rb,m.count,std::back_inserter(stacks[m.destination]));
+        stacks[m.source].resize(stacks[m.source].size()-m.count);
+    }
+};
+
+
+
+std::istream& operator>>(std::istream& inp,Crane& crane){
+    crane=Crane();
+    
+    struct inner{
+        static std::string readFour(std::istream& inp){
+            std::string out="aaaa";
+            for(int i=0;i<4;i++) out[i]=inp.get();        
+            return out;
+        }
+
+        static bool readOneLine(Crane& crane,std::istream& inp){
+            for(int i=0;true;i++){
+                auto g=readFour(inp);
+                if(g=="    "){
+                    continue;
+                }
+                if(g[0]=='['){
+                    if(i >= crane.stacks.size()){
+                        crane.stacks.resize(i+1);
+                    }
+                    crane.stacks[i].push_back(g[1]);
+                }
+                else if(g[1] != ' '){
+                    return false;
+                }
+                if(g[3]=='\n'){
+                    return true;
+                }
+            }
+        }
+    };
+
+    while((inner::readOneLine(crane,inp))){}
+    std::string tmp;
+    getline(inp,tmp);
+    for(auto& st : crane.stacks){
+        std::reverse(st.begin(),st.end());
+    }
+    
+    return inp;
+}
+std::ostream& operator<<(std::ostream& out,const Crane& crane){
+    
+    size_t stackHeight=std::accumulate(crane.stacks.begin(),crane.stacks.end(),0,
+        [](size_t T,const std::vector<char>& st)
+        { 
+            return std::max(T,st.size());
+        });
+    
+
+    for(size_t h=stackHeight;h>0;h--){
+        for(size_t i=0;i<crane.stacks.size();i++){
+            if((h-1) < crane.stacks[i].size()){
+                out << "[" << crane.stacks[i][h-1] << "] ";
+            }
+            else {
+                out << "    ";
+            }
+        }
+        out << "\n";
+    }
+
+    return out;
+}
+
+
+
+const std::string INPUTFILE="../day05/input.in";
+const std::string TESTFILE="../day05/test.in";
 
 int main(int argc,char** argv){
     std::ifstream input(INPUTFILE);
-    std::istream_iterator<Assignment> ing(input),end;
-    auto tot=std::accumulate(ing,end,0,[](int tot,const Assignment& g){
-        size_t sus=g[0].overlap(g[1]) || g[1].overlap(g[0]);
-        return tot+=sus;
-    });   
-    std::cout << tot << std::endl;
+    Crane crane;
+    input >> crane;
+    std::cout << crane << std::endl;
+    std::istream_iterator<Move> mi(input),mie;
+    for_each(mi,mie,[&crane](const Move& m){
+        crane.apply(m);
+        std::cout << crane << std::endl;
+    });
+    for(auto& st : crane.stacks){
+        std::cout << st.back();
+    }
+    std::cout << std::endl;
     return 0;
 }
