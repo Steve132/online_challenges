777caf20d06e4df0d4945f52fbc2347b7f869a71
Steve132 <steve@soapforge.com>
2023-01-23 18:27:09 -0500

    day10a

--- aoc2022/day10/main_a.cpp
+++ aoc2022/day10/main_a.cpp
@@ -56,7 +56,7 @@ public:
 
         if(execute(ncmd)){
             rprogram.pop_back();
-            std::cout << "ts: " << clock() << " executed " << ncmd << " X:" << value() << std::endl; 
+            //std::cout << "ts: " << clock() << " executed " << ncmd << " X:" << value() << std::endl; 
         }
         else{
             ncmd.waited++;
@@ -77,16 +77,26 @@ const std::string INPUTFILE="../day10/input.in";
 const std::string TESTFILE="../day10/test2.in";
 
 int main(int argc,char** argv){
-    std::ifstream input(TESTFILE);
+    std::ifstream input(INPUTFILE);
 
     std::istream_iterator<Command> inmoves(input),endmoves;
     Device dev{inmoves,endmoves};
 
+    size_t tot=0;
     for(size_t c=0;dev.tick();c++){
         if(c % 40 == 18){
-            std::cout << "X[" << c << "]==" << dev.value() << std::endl;
+            //std::cout << "X[" << c << "]==" << dev.value() << std::endl;
+            size_t ci=c+2;
+            size_t bi=(ci-20)/40;
+            
+            size_t cc=bi*40+20;
+            size_t v=dev.value();
+            size_t x=v*cc;
+            std::cout << "cc:" << cc << "" << " v:" << v << " x:" << x << std::endl;
+            tot+=x;
         }
     }
+    std::cout << "tot:" << tot << std::endl;
     
     return 0;
 }
